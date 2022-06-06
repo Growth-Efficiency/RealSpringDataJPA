ab724a9da67f573913557cefd774dc1d6700fe53
dongHyo <kdhyo98@gmail.com>
2022-06-06 10:48:54 +0900

    feat: 벌크성 수정 쿼리

--- kdhyo/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ kdhyo/data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -153,13 +153,29 @@ class MemberRepositoryTest {
 		// then
 		List<Member> content = page.getContent();
 		long totalElements = page.getTotalElements();
-		
+
 		assertThat(content).hasSize(3);
 		assertThat(totalElements).isEqualTo(5);
 		assertThat(page.getNumber()).isZero();
 		assertThat(page.getTotalPages()).isEqualTo(2);
 		assertThat(page.isFirst()).isTrue();
 		assertThat(page.hasNext()).isTrue();
+	}
+
+	@Test
+	void bulkUpdate() {
+		// given
+		memberRepository.save(new Member("member1", 10));
+		memberRepository.save(new Member("member2", 19));
+		memberRepository.save(new Member("member3", 20));
+		memberRepository.save(new Member("member4", 21));
+		memberRepository.save(new Member("member5", 40));
+
+		// when
+		int resultCount = memberRepository.bulkAgePlus(20);
+
+		// then
+		assertThat(resultCount).isEqualTo(3);
 	}
 
 }
