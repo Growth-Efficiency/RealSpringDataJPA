0ceb9fcf275922386620e1daaf31ec7c015262a5
JiYoul <wldbf97@naver.com>
2022-06-28 21:14:08 +0900

    [1일차] 순수 JPA 기반 레포지토리 만들어봄.

--- jiyoul/data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepostoryTest.java
+++ jiyoul/data-jpa/data-jpa/src/test/java/study/datajpa/repository/MemberRepostoryTest.java
@@ -8,6 +8,9 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -30,4 +33,32 @@ class MemberRepostoryTest {
         Assertions.assertThat(findMember).isEqualTo(member);
     }
 
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepostory.save(member1);
+        memberRepostory.save(member2);
+
+        //단건 조회 검증
+        Member findMember1 = memberRepostory.findById(member1.getId()).get();
+        Member findMember2 = memberRepostory.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        //리스트 조회 검증
+        List<Member> all = memberRepostory.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        //카운트 검증
+        long count = memberRepostory.count();
+        assertThat(count).isEqualTo(2);
+
+        //삭제 검증
+        memberRepostory.delete(member1);
+        memberRepostory.delete(member2);
+
+        long deletedCount = memberRepostory.count();
+        assertThat(deletedCount).isEqualTo(0);
+    }
 }
